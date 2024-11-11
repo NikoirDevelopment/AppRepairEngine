28c2fa80c2c138fbe408a94f191815c2f3691c42
NikoirDevelopment <hlebushek0612@ya.ru>
2024-11-12 05:02:33 +0900

    v0.0.1
    Создание окна авторизация и добавление контейнера Frame для переходов между страницами, а также создание подключение к базе данных MsSQL

--- DevelopmentRepairEngine/MainWindow.xaml.cs
+++ DevelopmentRepairEngine/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using DevelopmentRepairEngine.data.script;
+using DevelopmentRepairEngine.pages;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +25,8 @@ namespace DevelopmentRepairEngine
         public MainWindow()
         {
             InitializeComponent();
+            ControlHelper.frmobj = FrmMain;
+            //OdbConnectionHelper.Connect
         }
     }
 }
